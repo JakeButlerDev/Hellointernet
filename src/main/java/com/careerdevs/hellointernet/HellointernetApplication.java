3751ce153b2a2511bb5e28b1d43be23ee0c77e44
Jake Butler <JakeButler.student@careerdevs.com>
2022-07-26 18:24:55 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/hellointernet/HellointernetApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.hellointernet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellointernetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellointernetApplication.class, args);
+	}
+
+}
