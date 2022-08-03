7253499c9dcf59c84c3596a32a14510baea7f061
Jake Butler <JakeButler.student@careerdevs.com>
2022-08-02 21:32:04 -0400

    Added randomnumber route, fixed date route

--- src/main/java/com/careerdevs/hellointernet/controllers/NasaController.java
+++ src/main/java/com/careerdevs/hellointernet/controllers/NasaController.java
@@ -1,9 +1,7 @@
 package com.careerdevs.hellointernet.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 // First step is to add the RestController and RequestMapping annotations to the NasaController class
@@ -11,8 +9,11 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/nasa")
 public class NasaController {
 
+    @Value("${myNasaKey}")
+    private String myNasaKey;
+
     //Second step is to create a nasaApodEndpoint field within your new class.
-    private final String myNasaKey = "HRnjNYo4665YLMXBeNbDzuhhAdZMdWMyhxhSs4QD";
+//    private final String myNasaKey = "HRnjNYo4665YLMXBeNbDzuhhAdZMdWMyhxhSs4QD";
 
     private final String nasaApodEndpoint = "https://api.nasa.gov/planetary/apod?api_key=" + myNasaKey;
 
@@ -24,13 +25,24 @@ public class NasaController {
 
     // Fourth step is to make a request route that allows you to change the date of the APOD information being requested. You’ll need to use either @PathVariable or @RequestParam, either will work.
     @GetMapping("/{date}")
-    private void getByDate(@RequestParam String date) {
-
+    public Object getByDate(RestTemplate restTemplate, @PathVariable String date) {
+        String nasaApodDate = nasaApodEndpoint;
+        nasaApodDate += "&date=" + date;
+        return restTemplate.getForObject(nasaApodDate, Object.class);
     }
 
     // Fourth request to build is a GET request that will return a collection of 5 randomly selected APOD pictures. You’ll need to reference the APOD documentation linked at the top of this page. There’s a specific APOD endpoint you can hit to get a random amount of images.
+    @GetMapping("/randomnumber")
+    public Object getRandomCount(RestTemplate restTemplate) {
+        int count = 5;
+        String randomizeApod = nasaApodEndpoint;
+        randomizeApod += "&count=" + count;
+        return restTemplate.getForObject(randomizeApod, Object.class);
+    }
 
-
+    //TODO: Error handling - 400 bad request when user enters invalid date, FURTHER - print to client with msg provided from server
 
     //TODO: BONUS - Learn how to store your API Key secretly in the application.properties file. (learn about environment variables)
+
+    // Can try to hide in application.properties, if unable to work go for .gitignore
 }
